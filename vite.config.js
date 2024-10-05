b1ac9fd9132ec92f02f32dbfafff920395c7361a
st-yana <sternadjana8@gmail.com>
2024-10-05 17:42:49 +0200

    Fix file paths; fix package json /REPO/ value

--- vite.config.js
+++ vite.config.js
@@ -37,7 +37,6 @@ export default defineConfig(({ command }) => {
       outDir: '../dist',
       emptyOutDir: true,
     },
-    
     plugins: [
       injectHTML(),
       FullReload(['./src/**/**.html']),
